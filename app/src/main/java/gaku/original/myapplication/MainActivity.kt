40d45817fe619c484795d7e04def649910a79f13
GakuAkita <gaku.medaka@gmail.com>
2024-10-19 16:08:30 +0900

    initial commit

---
+++ app/src/main/java/gaku/original/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package gaku.original.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import gaku.original.myapplication.ui.theme.HouseholdExpensesTheme
+import gaku.original.myapplication.ui.theme.MainView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HouseholdExpensesTheme {
+                // 一番最初にデフォルで存在するScaffold
+                // HedgehogだとデフォルトでSurfaceがあってやりやすかったのでそっちをパクる。
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface(modifier = Modifier.padding(innerPadding)) {
+                        //ここにいろいろ作っていく
+                        MainView()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/gaku/original/myapplication/ui/theme/MainView.kt
@@ -0,0 +1,23 @@
+package gaku.original.myapplication.ui.theme
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainView(){
+//    Scaffold(
+//        topBar = {
+//            TopAppBar(
+//                title={ Text("What is essential is invisible to the eye.") }
+//            )
+//        },
+//    ){
+//        innerPadding ->
+//        Text("This is Main", modifier = Modifier.padding(innerPadding))
+//    }
+}
