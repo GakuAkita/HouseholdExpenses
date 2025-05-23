b952d71c697697d8a838feb75367a6ab8994e43d
GakuAkita <gaku.medaka@gmail.com>
2025-05-24 08:34:14 +0900

    Replaced Log with LogAkitaDebug

--- app/src/main/java/gaku/original/myapplication/MainActivity.kt
+++ app/src/main/java/gaku/original/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package gaku.original.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +9,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
+import gaku.original.myapplication.Utility.LogAkitaDebug
 import gaku.original.myapplication.ui.theme.HouseholdExpensesTheme
 
 @AndroidEntryPoint
@@ -38,35 +38,35 @@ class MainActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
         //起動時になにかしたいとき
-        Log.d("AkitaLearningAndroidAppCore", "onStart() called. Do nothing.")
+        LogAkitaDebug("onStart() called. Do nothing.")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("AkitaLearningAndroidAppCore", "onResume() called. Do nothing.")
+        LogAkitaDebug("onResume() called. Do nothing.")
     }
 
     override fun onPause() {
         super.onPause()
         //一時停止時になにかしたいとき。ユーザーは見えている
-        Log.d("AkitaLearningAndroidAppCore", "onPause() called. Do nothing.")
+        LogAkitaDebug("onPause() called. Do nothing.")
     }
 
     override fun onStop() {
         super.onStop()
         //アプリ終了時になにかしたいとき。ユーザーは何も見えない。
-        Log.d("AkitaLearningAndroidAppCore", "onStart() called. Do nothing.")
+        LogAkitaDebug("onStart() called. Do nothing.")
     }
 
     override fun onRestart() {
         super.onRestart()
         //再起動時になにかしたいとき
-        Log.d("AkitaLearningAndroidAppCore", "onRestart() called. Do nothing.")
+        LogAkitaDebug("onRestart() called. Do nothing.")
     }
 
     override fun onDestroy() {
         super.onDestroy()
         //破棄時になにかしたいとき
-        Log.d("AkitaLearningAndroidAppCore", "onDestroy() called. Do nothing.")
+        LogAkitaDebug("onDestroy() called. Do nothing.")
     }
 }
