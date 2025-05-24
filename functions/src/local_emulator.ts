948ed311342916f2e332354c0ad2cb8f4f8605fe
GakuAkita <gaku.medaka@gmail.com>
2025-05-24 21:03:15 +0900

    I can connect to the Firestore emulator and perform operations.

--- functions/src/local_emulator.ts
+++ functions/src/local_emulator.ts
@@ -1,23 +1,43 @@
 import * as dotenv from "dotenv";
 import * as path from "path";
-import { getMyFirestore } from "./myFunc/firebaseAdmin";
+import { FirestoreService } from "./FirestoreService/FirestoreService";
+import { Category } from "./type/Category";
+import { Expense } from "./type/Expense";
 
 // 環境変数を読み込む
 dotenv.config({ path: path.resolve(__dirname, "../../.env.local") });
 
+const service = new FirestoreService({
+  projectId: process.env.FIREBASE_PROJECT_ID, // 環境変数からprojectIdを取得
+});
+
 const main = async () => {
-  console.log("Firestore emulator ready");
+  const userId: string = "testUser";
 
-  const db = getMyFirestore({
-    projectId: process.env.FIREBASE_PROJECT_ID, // 環境変数からprojectIdを取得
-  });
-  console.log("Firestore instance created:", db);
+  const sampleCategory: Category = {
+    id: "category1",
+    timestamp: Date.now(),
+    name: "Food",
+    enabled: true,
+  };
+  const sampleExpense: Expense = {
+    timestamp: Date.now(),
+    amount: 100,
+    category: sampleCategory,
+  };
+  service.addExpenseWithId(userId, sampleExpense);
 
-  // 例: データ追加
-  await db.collection("test").add({
-    message: "Hello from emulator",
-    timestamp: new Date(),
-  });
+  const sampleRepeatAdd = {
+    id: "repeatAdd1",
+    timestamp: Date.now(),
+    expense: sampleExpense,
+    frequencyInfo: {
+      frequency: "weekly",
+      dayOfWeek: ["Monday", "Wednesday"],
+      hour: 10,
+      minute: 30,
+    },
+  };
 
   console.log("Data written to emulator.");
 };
---
+++ functions/src/type/FuncStatus.ts
@@ -0,0 +1,14 @@
+export enum FuncStatus {
+  SUCCESS = "success",
+  TIMEOUT = "timeout",
+  ERROR = "error",
+}
+
+export interface FuncResult {
+  status: FuncStatus;
+  message?: string;
+}
+
+export interface FirestoreAddResult extends FuncResult {
+  id?: string; // 成功時に追加されたドキュメントのID
+}
