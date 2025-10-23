e8b60af7aa1e451f04aecf419997cb0e1688404c
GakuAkita <gaku.medaka@gmail.com>
2025-10-23 17:23:39 +0900

    Replaced NOT_CATEGORIZED with SEARCH

--- app/src/main/java/gaku/original/myapplication/ui/view/main/AddEditView.kt
+++ app/src/main/java/gaku/original/myapplication/ui/view/main/AddEditView.kt
@@ -97,7 +97,7 @@ FloatingActionボタンから来た場合は、ボタンを叩いた時間を入
 カレンダーの日付を叩いてきたときはその日付と時間(今の時間)をデフォルトでいれる
  */
 enum class FromScreen {
-    NOT_CATEGORIZED,
+    SEARCH,
     MAIN_CONTENT,
     UNKNOWN
 }
@@ -111,7 +111,7 @@ fun ExpenseAddEditView(
     from: String/* 遷移元のスクリーン */
 ) {
     val fromScreen = when (from) {
-        Screen.SearchScreen.route -> FromScreen.NOT_CATEGORIZED
+        Screen.SearchScreen.route -> FromScreen.SEARCH
         Screen.MainScreen.Content.route -> FromScreen.MAIN_CONTENT
         else -> FromScreen.UNKNOWN
     }
@@ -582,7 +582,7 @@ fun ExpenseAddEditView(
                             label = { Text(text = "商品名(空欄可)") },
                             modifier = basicModifier
                         )
-                        if (fromScreen == FromScreen.NOT_CATEGORIZED) {
+                        if (fromScreen == FromScreen.SEARCH) {
                             if (mainType == GeneratedType.MAIL_EXTRACTION && subType != null) {
                                 val templateType = getEmailTemplateTypeByNodeName(subType)
                                 if (templateType != null) {
@@ -652,7 +652,7 @@ fun ExpenseAddEditView(
                  * EmailTemplateTypeにはcategoryAssignFlagがあって、そこにビットで商品名なのか店名なのかを保持している
                  * AND演算子を使って店名や商品名が入っているかをとり、入っていたらボタンを表示
                  * */
-                if (fromScreen == FromScreen.NOT_CATEGORIZED) {
+                if (fromScreen == FromScreen.SEARCH) {
                     if (mainType == GeneratedType.MAIL_EXTRACTION && subType != null) {
                         val templateType = getEmailTemplateTypeByNodeName(subType)
                         if (templateType != null) {
