ebbb84a5dcc28a65d46c3e59a28a912a71caa3b4
GakuAkita <gaku.medaka@gmail.com>
2026-08-23 16:43:51 +0900

    Remove BottomBar from VersionScreen
    - Removed the `BottomBarView` component from the `Scaffold` implementation in `VersionScreen.kt`.
    - Cleaned up redundant whitespace and empty lines in the layout definition.

--- app/src/main/java/gaku/original/myapplication/di/appContainer/FirebaseAuthTestAppContainer.kt
+++ app/src/main/java/gaku/original/myapplication/di/appContainer/FirebaseAuthTestAppContainer.kt
@@ -1,15 +1,13 @@
 package gaku.original.myapplication.di.appContainer
 
-import android.content.Context
-import gaku.original.myapplication.data.repository.auth.AuthRepository
 import com.google.firebase.auth.FirebaseAuth
+import gaku.original.myapplication.data.repository.auth.AuthRepository
 import gaku.original.myapplication.data.repository.auth.FirebaseAuthRepository
 import gaku.original.myapplication.data.repository.emailConnect.EmailConnectionRepositoryFirebase
 import gaku.original.myapplication.di.sessionContainer.FakeSessionContainer
 import gaku.original.myapplication.di.sessionContainer.SessionContainer
-import gaku.original.myapplication.ui.screens.start.signin.GoogleCredentialProvider
 
-class FirebaseAuthTestAppContainer: FakeAppContainer() {
+class FirebaseAuthTestAppContainer : FakeAppContainer() {
     val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
     override val authRepository: AuthRepository = FirebaseAuthRepository(
         firebaseAuth = firebaseAuth
